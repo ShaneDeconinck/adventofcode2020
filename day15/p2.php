9cd57c00810a678c2c881fbf06cb7b74776a49bf
Shane Deconinck <shane.deconinck@gmail.com>
2020-12-16 00:47:27 +0100

    day 15

--- day15/p2.php
+++ day15/p2.php
@@ -2,29 +2,33 @@
 
 $start = microtime(true);
 
-$input = "14,3,1,0,9,5";
-$array = explode(",", $input);
-$iterator = count($array)-1;
+
+$array = explode(",", "14,3,1,0,9,5");
 
 $pos = [];
 foreach($array as $k=>$v) {
   $array[$k] = (int)$v;
-  $pos[(int)$v] = (int)$k;
+  $pos[(int)$v] = $k;
 }
 
-$next = $array[count($array)-1];
+$iterator = count($array)-1;
+$next = $array[$iterator];
 
-while ($iterator < (30000000 - 1)) {
+while ($iterator < 30000000 - 1) {
+  
   	$current = $next;
-  	if(array_key_exists((int)$current, $pos)){
+    $next = 0;
+  
+  	if(array_key_exists($current, $pos)){
       $next = $iterator - $pos[$current];
-    } else {
-    	$next = 0;
     }
+  
     $pos[$current] = $iterator;
 	$iterator++;
+  
 }
+
+echo $next;
 
 $time_elapsed_secs = microtime(true) - $start;
 echo $time_elapsed_secs . "sec \n";
-echo $next;
